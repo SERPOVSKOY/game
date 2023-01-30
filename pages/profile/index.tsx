c48f5da90917e3043f86b38d2394fc83fc7d74de
Aleksei_Serpovskoi <as.serpovskoy@mail.ru>
2023-01-30 22:21:41 +0300

    draft: [#2] add deps and config eslint (no working)

---
+++ pages/profile/index.tsx
@@ -0,0 +1,14 @@
+const ProfilePage = () => {
+  const a = 123;
+
+
+
+  return (
+    <div>
+      <h1>Серповской Алексей Сергеевич</h1>
+      <p>Front-end developer</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
